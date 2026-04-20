8222646d5b18f622aceec882f9371a191f94e0bc
Walysson Campos <walyssoncamposxy@gmail.com>
2026-04-20 20:15:47 -0300

    Atualização do dia 20/04

--- porta/script.js
+++ porta/script.js
@@ -137,25 +137,34 @@ document.getElementById("formPorta").addEventListener("submit", function(e) {
 
   const area = (largura/100) * (altura/100);
   const valor = area * precoMetro;
+  
 
-  document.getElementById("resultado").innerHTML = `Valor estimado: R$ ${valor.toFixed(2)} \n
-  <small>*Valor sujeito à visita técnica. O valor da instalação não está incluso.</small> \n\n
-  <a href="${gerarWhatsApp(valor)}" target="_blank">
+  document.getElementById("resultado").innerHTML = `Valor estimado: R$ ${valor.toFixed(2)}` +
+  `<p>Valor sujeito à visita técnica.</p>` + 
+  `<p>O valor da instalação não está incluso.</p>` +
+  `<a href="${gerarWhatsApp(valor)}" target="_blank">
     Enviar pelo WhatsApp
   </a>`;
 });
 
 function gerarWhatsApp(valor){
     const largura = document.getElementById("largura").value;
+    const largura_metro = (largura/100);
     const altura = document.getElementById("altura").value;
-    const tipoPorta = document.getElementById("tipoPorta").value;
-    const tipoVidro = document.getElementById("tipoVidro").value;
+    const altura_metro = (altura/100);
+    const selectPorta = document.getElementById("tipoPorta");
+    const tipoPorta = selectPorta.options[selectPorta.selectedIndex].text;
+    const selectVidro = document.getElementById("tipoVidro");
+    const tipoVidro = selectVidro.options[selectVidro.selectedIndex].text;
+    const selectCor = document.getElementById("corVidro");
+    const corVidro = selectCor.options[selectCor.selectedIndex].text;
 
-    const mensagem = `Olá! Gostaria de um orçamento:
-    Porta: ${tipoPorta} \n
-    Medidas: ${largura}cm x ${altura}cm \n
-    Vidro: ${tipoVidro} \n
-    Valor estimado: R$ ${valor.toFixed(2)}`;
+     const mensagem = `Olá! Gostaria de um orçamento:\n\n` +
+    `Porta: ${tipoPorta}\n` + 
+    `Medidas: ${largura_metro}m x ${altura_metro}m\n` +
+    `Vidro: ${tipoVidro}\n` +
+    `Cor: ${corVidro}\n` +
+    `Valor estimado: R$ ${valor.toFixed(2)}`;
 
-    return `https://wa.me/5532998127285?text=${encodeURIComponent(mensagem)}`;
+    return `https://wa.me/55SEUNUMERO?text=${encodeURIComponent(mensagem)}`;
 }
